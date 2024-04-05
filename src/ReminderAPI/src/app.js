6516efbf64d2656789bb1bf7849f061e8add4957
alvarosoaress <alvarosoares01@gmail.com>
2024-04-05 18:07:50 -0300

    fix: vercel deploy #4

--- src/ReminderAPI/src/app.js
+++ src/ReminderAPI/src/app.js
@@ -1,13 +1,19 @@
 require('./db-connection.js');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(process.cwd(), './src/.env') }); // Carrega as variáveis de ambiente do arquivo .env
 const express = require('express');
 const bodyParser = require('body-parser');
 const notesRoutes = require('./routes/notesRoutes');
 const userRoutes = require('./routes/userRoutes');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const path = require('path');
 
-const swaggerYaml = path.resolve(process.cwd(), './swagger.yaml');
+const developEnv = process.env.DEVELOP;
+
+const swaggerYaml = path.resolve(
+  process.cwd(),
+  `${developEnv ? './swagger.yaml' : './src/ReminderAPI/swagger.yaml'}`,
+);
 
 const swaggerDocument = YAML.load(swaggerYaml);
 
