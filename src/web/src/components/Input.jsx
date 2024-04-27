d16c6e82775fbcc2614574380062fab7db3bd857
alvarosoaress <alvarosoares01@gmail.com>
2024-04-27 14:23:31 -0300

    fix(api): vercel cors #3

--- src/web/src/components/Input.jsx
+++ src/web/src/components/Input.jsx
@@ -5,19 +5,20 @@ export default function Input({
   min,
   required,
   name,
+  disabled,
   error,
   ...props
 }) {
   return (
     <input
-      // onInput={(e) => setState(e.target.value)}
       {...props}
       required={required}
       type={type}
       max={max ?? ''}
       min={min ?? ''}
       name={name ?? ''}
-      className={`${props.className} p-1 border-[1px] border-gray-600 outline-gray-600 border-solid focus:rounded-md w-${width} lg:w-72 lg:h-12 ${error && 'border-red-600 outline-red-600'} `}
+      disabled={disabled}
+      className={`${props.className} p-1 border-[1px] border-gray-600 outline-gray-600 border-solid focus:rounded-md w-${width} lg:w-72 lg:h-12 ${error && 'border-red-600 outline-red-600'} disabled:bg-slate-200 disabled:rounded-md `}
     />
   );
 }
--- src/web/src/pages/Login.jsx
+++ src/web/src/pages/Login.jsx
@@ -2,33 +2,67 @@ import { useState } from 'react';
 import Input from '../components/Input';
 import Title from '../components/Title';
 import Logo from '../assets/images/logo.png';
+import axios from 'axios';
 
 export default function Login() {
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  function handleSubmit(e) {
-    var formData = new FormData(e.target);
-    let values = Object.fromEntries(formData);
-    let erros = {};
+  async function handleSubmit(e) {
+    const formData = new FormData(e.target);
+    const values = Object.fromEntries(formData);
+    const errors = {};
+
+    setLoading(true);
 
     if (!values.email && !values.password) {
-      erros.general = 'Preencha todos os campos.';
+      errors.general = 'Preencha todos os campos.';
     } else if (values.email.length < 3) {
-      erros.email = 'O email deve ter no mínimo 3 caracteres.';
+      errors.email = 'O email deve ter no mínimo 3 caracteres.';
     } else if (
-      !values.email
-        .toLowerCase()
-        .match(
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        )
+      !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(values.email)
     ) {
-      erros.email = 'O email deve ser válido.';
+      errors.email = 'O email deve ser válido.';
     } else if (values.password.length < 6) {
-      erros.password = 'A senha deve ter no mínimo 6 caracteres.';
+      errors.password = 'A senha deve ter no mínimo 6 caracteres.';
     }
 
-    setError(erros);
-    console.log(values);
+    if (Object.keys(errors).length > 0) {
+      setError(errors);
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_URL}/users/login`,
+        values,
+      );
+
+      console.log(res);
+      setLoading(false);
+      return;
+    } catch (err) {
+      if (err.response.status == 404) {
+        errors.general = 'Usuário ou senha inválidos.';
+        console.error(err);
+        setError(errors);
+        setLoading(false);
+        return;
+      } else if (err.response.status == 400) {
+        errors.general = 'Usuário não encontrado.';
+        console.error(err);
+        setError(errors);
+        setLoading(false);
+        return;
+      } else {
+        errors.general = 'Algo deu errado, tente novamente.';
+        console.error(err);
+        setError(errors);
+        setLoading(false);
+        return;
+      }
+    }
   }
 
   return (
@@ -56,6 +90,7 @@ export default function Login() {
               min={3}
               type="email"
               name="email"
+              disabled={loading}
             />
             <small className="text-red-500 text-md">{error.email}</small>
           </div>
@@ -66,11 +101,15 @@ export default function Login() {
               error={error.password || error.general}
               type="password"
               name="password"
+              disabled={loading}
             />
             <small className="text-red-500 text-md">{error.password}</small>
           </div>
 
-          <button className="bg-[#317BE9] text-white font-semibold w-52 h-12 hover:scale-110 hover:rounded-md mt-6 lg:w-72">
+          <button
+            disabled={loading}
+            className="bg-[#317BE9] text-white font-semibold w-52 h-12 hover:scale-110 hover:rounded-md mt-6 disabled:bg-slate-400 disabled:hover:scale-100 disabled:rounded-md lg:w-72"
+          >
             Continuar
           </button>
 
