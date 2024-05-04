3a33953b0aca18c5a1f3066408ce65d7a32b3dbd
André Azevedo <aazevedo525@gmail.com>
2024-05-04 20:29:22 -0300

    Adicao de rotas da pagina Usuario

--- src/web/src/routes/index.jsx
+++ src/web/src/routes/index.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
 import Home from '../pages/Home';
+import Usuario from '../pages/Usuario';
 
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFound />} />
+      <Route path="/u" element={<Usuario />} />
     </Routes>
   );
 }
