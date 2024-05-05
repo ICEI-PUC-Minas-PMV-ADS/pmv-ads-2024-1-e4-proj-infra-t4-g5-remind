7c33a71d4f59f788b85fcf11eb8419e78b96ede3
creinis <115181527+creinis@users.noreply.github.com>
2024-05-05 18:56:24 +0200

    fix:css input register text

--- src/webCommerce/src/components/Register.jsx
+++ src/webCommerce/src/components/Register.jsx
@@ -23,20 +23,20 @@ const Register = ({ onButtonClick }) => {
         <div className='relative flex flex-col items-center justify-center rounded-2xl bg-zinc text-neutral-100 p-8 shadow-lg'>
 
         </div>
-      <form className='relative flex flex-col items-center justify-center rounded-2xl border border-purple-500 bg-zinc text-neutral-100 p-8 shadow-lg'>
+      <form className='relative flex flex-col items-center justify-center rounded-2xl text-neutral-100 border border-purple-500 bg-zinc  p-8 shadow-lg'>
       <div className='text-neutral-100 text-2xl pb-4'>
         <h2>Registre-se</h2>
       </div>
-        <div className=' pb-4'>
-          <label htmlFor="userName" className='text-sm'>Nome</label>
-          <input className='w-full' type="text" id="userName" name="userName" required onChange={(e) => setUserName(e.target.value)} />
+        <div className='text-black pb-4'>
+          <label htmlFor="userName" className='text-neutral-100 text-sm'>Nome</label>
+          <input className='w-full' type="text" id="Name" name="Name" required onChange={(e) => setUserName(e.target.value)} />
         </div>
-        <div className=' pb-4'>
-          <label htmlFor="email" className='text-sm'>E-mail</label>
+        <div className='text-black pb-4'>
+          <label htmlFor="email" className='text-neutral-100 text-sm'>E-mail</label>
           <input className='w-full' type="email" id="email" name="email" required onChange={(e) => setEmail(e.target.value)} />
         </div>
-        <div className='pb-4'>
-          <label htmlFor="password" className='text-sm'>Senha</label>
+        <div className='text-black pb-4'>
+          <label htmlFor="password" className='text-neutral-100 text-sm'>Senha</label>
           <input className='w-full' type={showPassword ? "text" : "password"} id="password" name="password" required onChange={(e) => setPassword(e.target.value)} />
           <div className="flex justify-end mt-2">
           <button className='flex justify-end text-xs text-purple-400 pb-4' type="button" onClick={() => setShowPassword(!showPassword)}>
