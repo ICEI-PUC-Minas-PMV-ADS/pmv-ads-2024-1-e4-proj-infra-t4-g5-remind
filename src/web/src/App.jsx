d4f8c99014b1975751a4c3e65226cddaf7d26616
alvarosoaress <alvarosoares01@gmail.com>
2024-05-05 22:53:29 -0300

    feat(web): better cards and createNote function

--- src/web/src/App.jsx
+++ src/web/src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './routes';
 import UserProvider from './context/UserContext';
+import { Toaster } from 'react-hot-toast';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <div>
+        <Toaster />
+      </div>
       <UserProvider>
         <Router />
       </UserProvider>
--- src/web/src/components/TopicItem.jsx
+++ src/web/src/components/TopicItem.jsx
@@ -9,10 +9,12 @@ export default function TopicItem({
   return (
     <button
       {...rest}
-      className={`flex items-center gap-2 cursor-pointer group`}
+      className={`flex items-center gap-2 cursor-pointer group relative`}
       onClick={() => onClick && onClick()}
     >
-      {active ? <div className="rounded-[50%] bg-primary w-1 h-1"></div> : null}
+      {active ? (
+        <div className="rounded-[50%] bg-primary w-1 h-1 absolute -left-2"></div>
+      ) : null}
       {Icon ? <Icon active={active} /> : null}
       <h2
         className={`text-textSecondary group-hover:text-primary group-hover:font-semibold ${open != null && !open && 'hidden'} ${active && '!font-semibold !text-primary'}`}
