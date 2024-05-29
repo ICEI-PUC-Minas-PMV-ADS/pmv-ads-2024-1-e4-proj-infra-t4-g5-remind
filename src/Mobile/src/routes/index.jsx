0f671869f5aa5e0e4a786a11d2c0df04668e0e24
André Azevedo <aazevedo525@gmail.com>
2024-05-28 22:56:20 -0300

    Update index.jsx
    Adição de rotas para as telas

--- src/Mobile/src/routes/index.jsx
+++ src/Mobile/src/routes/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
+
 // Importe suas telas
-import Login from '../pages/Login'
+import Login from '../pages/Login';
+import Home from '../pages/Home';
+import Header from '../components/Header';
 
 
 const Stack = createNativeStackNavigator();
 
+
 export default function Router() {
   return (
-    <Stack.Navigator initialRouteName='Login'>
-      <Stack.Screen name="Login" component={Login} />
+    <Stack.Navigator
+      screenOptions={({ route }) => ({
+        header: () => <Header title={route.name} />,
+      })}
+    >
+      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}  />
+      <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   );
 }
