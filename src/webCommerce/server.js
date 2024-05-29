43000293624afd58e09fec406fcdcd42e455e7c7
creinis <115181527+creinis@users.noreply.github.com>
2024-05-29 15:10:38 +0200

    update: webCom: email: not unique

--- src/webCommerce/server.js
+++ src/webCommerce/server.js
@@ -5,9 +5,9 @@ import helmet from 'helmet';
 import express from 'express';
 import bodyParser from 'body-parser';
 import connectDB from './db-connection.js';
-import purchaseRoutes from './src/routes/purchaseRoutes.js';
-import paymentRoutes from './src/routes/paymentRoutes.js';
-import subscriptionStatusRoutes from './src/routes/subscriptionStatusRoutes.js';
+import Purchase from './src/models/PurchaseSchema.js';
+import Payment from './src/models/PaymentSchema.js';
+import SubscriptionStatus from './src/models/SubscriptionStatusSchema.js';
 
 const __dirname = path.resolve();
 dotenv.config({ path: path.join(__dirname, './.env') });
@@ -59,16 +59,59 @@ app.use(helmet({
   permittedCrossDomainPolicies: { permittedPolicies: 'none' },
   referrerPolicy: { policy: 'no-referrer' },
   xssFilter: true,
-}));
+} ));
 
 app.use(bodyParser.json());
 
 app.options('*', cors()); // Enable pre-flight requests for all routes
 
-// Rotas
-app.use('/api', purchaseRoutes);
-app.use('/api', paymentRoutes);
-app.use('/api', subscriptionStatusRoutes);
+// Rota para criação de Purchase
+app.post('/purchase/', async (req, res) => {
+  console.log(`Rota usada: ${req.originalUrl}`);
+  
+  try {
+    const purchase = new Purchase(req.body);
+    await purchase.save();
+    console.log('Status da operação: 201 Created');
+    res.status(201).send(purchase);
+  } catch (error) {
+    console.log('Status da operação: 400 Bad Request', error);
+    res.status(400).send(error);
+  }
+});
+
+// Rota para criação de Payment
+app.post('/payment', async (req, res) => {
+  console.log(`Rota usada: ${req.originalUrl}`);
+  try {
+    const payment = new Payment(req.body);
+    await payment.save();
+    res.status(201).send(payment);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Rota para criação de SubscriptionStatus
+app.post('/subscription-status', async (req, res) => {
+  console.log(`Rota usada: ${req.originalUrl}`);
+  try {
+    const { purchase_id, payment_id } = req.body;
+
+    const status = purchase_id && payment_id ? true : false;
+
+    const subscriptionStatus = new SubscriptionStatus({
+      purchase_id,
+      payment_id,
+      status,
+    });
+
+    await subscriptionStatus.save();
+    res.status(201).send(subscriptionStatus);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => console.log(`CLIENT Server running on port ${PORT}`));
--- src/webCommerce/src/models/PurchaseSchema.js
+++ src/webCommerce/src/models/PurchaseSchema.js
@@ -50,7 +50,6 @@ const PurchaseSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
   },
   password: {
     type: String,
