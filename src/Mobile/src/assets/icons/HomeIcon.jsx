82ca0b353556fc702aa9ba5fe0acc0c39b928d2d
André Azevedo <aazevedo525@gmail.com>
2024-05-28 22:53:39 -0300

    Novos Assets

--- src/Mobile/src/assets/icons/HomeIcon.jsx
+++ src/Mobile/src/assets/icons/HomeIcon.jsx
@@ -1,23 +1,37 @@
-export default function HomeIcon({ active }) {
-  return (
-    <svg
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="svgPrimaryGroupHover"
-    >
-      <path
-        d="M17 9.65293V10.7938C17 13.7194 17 15.1822 16.1213 16.0911C15.2427 17 13.8284 17 11 17H8C5.17157 17 3.75736 17 2.87868 16.0911C2 15.1822 2 13.7194 2 10.7938V9.65293C2 7.93662 2 7.07846 2.3894 6.36705C2.7788 5.65566 3.49021 5.21413 4.91302 4.3311L6.41302 3.40015C7.91704 2.46672 8.66907 2 9.5 2C10.3309 2 11.0829 2.46672 12.587 3.40015L14.087 4.33109C15.5098 5.21413 16.2212 5.65566 16.6106 6.36705"
-        stroke={active ? '#4b0195' : '#656F7D'}
+import React from 'react';
+import Svg, { Path, G } from 'react-native-svg';
+
+const HomeIcon = ({ active }) => (
+  <Svg width={22} height={24} viewBox="0 0 22 24" fill="none">
+    <G id="Icon / home">
+      <Path
+        id="Path"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M1 8.7L11 1L21 8.7V20.8C21 22.015 20.0051 23 18.7778 23H3.22222C1.99492 23 1 22.015 1 20.8V8.7Z"
+        fill={active ? '#443368' : '#656F7D'}
+        stroke={active ? '#443368' : '#656F7D'}
+        strokeWidth="1.6"
         strokeLinecap="round"
+        strokeLinejoin="round"
       />
-      <path
-        d="M11.75 14H7.25"
-        stroke={active ? '#4b0195' : '#656F7D'}
-        strokeLinecap="round"
-      />
-    </svg>
-  );
-}
+      <G id="Path_2">
+        <Path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M7.66675 23V12H14.3334V23"
+          fill="white"
+        />
+        <Path
+          d="M7.66675 23V12H14.3334V23"
+          stroke="white"
+          strokeWidth="1.6"
+          strokeLinecap="square"
+          strokeLinejoin="round"
+        />
+      </G>
+    </G>
+  </Svg>
+);
+
+export default HomeIcon;
