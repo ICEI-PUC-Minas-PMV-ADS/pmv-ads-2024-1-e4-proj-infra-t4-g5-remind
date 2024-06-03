74cf409c7c5d6bc86a4f555c471822d1c1f83b13
creinis <115181527+creinis@users.noreply.github.com>
2024-06-03 21:19:00 +0200

    WebCom: fix: css userStories-container

--- src/webCommerce/src/pages/UserStories.jsx
+++ src/webCommerce/src/pages/UserStories.jsx
@@ -33,12 +33,12 @@ const UserStories = () => {
         <div className="flex flex-col justify-center items-center overflow-hidden">
           <div className="mt-12 mb-24">
             <h2 id="userStories_title" className="text-7xl lg:text-7xl font-semibold pb-2">Remind.</h2>
-            <h2 id="userStories_subtitle" className="section-heading text-5xl lg:text-5xl font-semibold pl-12 pt-1">Comunicação</h2>
-            <h2 id="userStories_subtitle" className="section-heading text-5xl lg:text-5xl font-semibold pl-24 pt-1">descomplicada</h2>
-            <h2 id="userStories_subtitle" className="section-heading text-5xl lg:text-5xl font-semibold pl-40 pt-1">para equipes.</h2>
+            <h2 id="userStories_subtitle" className="section-sub-heading pl-12">Comunicação</h2>
+            <h2 id="userStories_subtitle" className="section-sub-heading pl-24">descomplicada</h2>
+            <h2 id="userStories_subtitle" className="section-sub-heading pl-40">para equipes.</h2>
           </div>
-
-          <div className="flex-center flex-col sm:px-10">
+          
+          <div className="userStories-container">
 
             <div className="flex flex-col w-full relative">
               <div className="userStories-video-container">
@@ -46,8 +46,8 @@ const UserStories = () => {
 
                   {/* Persona-01 */}
                   <img src="https://images.unsplash.com/photo-1618088129969-bcb0c051985e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" 
-                  alt="titanium" className="userStories-video g_grow" />
-                    <div className="userStories-video-container g_grow flex-1 flex-center" 
+                  className="userStories-video g_grow" />
+                    <div className="userStories-video-container g_grow flex-1 flex-center first-line" 
                           style={{
                             position: 'absolute',
                             top: '50%',
@@ -64,15 +64,16 @@ const UserStories = () => {
                       </p>
                   </div>
                 </div>
-                <div className="overflow-hidden flex-1 h-[100vh]">
+                <div className="overflow-hidden flex-1 h-[100vh] relative">
                   
                   {/* Persona-03 */}
-                  <img src="https://images.unsplash.com/photo-1584853538847-f3405f62b905?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" alt="titanium 2" className="userStories-video g_grow" />
+                  <img src="https://images.unsplash.com/photo-1584853538847-f3405f62b905?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+                  className="userStories-video g_grow" />
                   <div className="userStories-video-container g_grow flex-1 flex-center" 
                           style={{
                             position: 'absolute',
-                            top: '50%',
-                            left: '61%',
+                            top: '60%',
+                            left: '10%',
                             right: '25%'
                           }}>
                       <p className="userStories-video text-zinc g_grow italic">
@@ -113,12 +114,8 @@ const UserStories = () => {
             </div>
           </div >
 
-          <div className="mb-12 w-full h-[15vh]">
-            
-          </div>
-
           {/* Second Block */}
-          <div className="flex-center flex-col sm:px-10">
+          <div className="userStories-container">
 
             <div className="flex flex-col w-full relative">
               <div className="userStories-video-container">
@@ -126,12 +123,12 @@ const UserStories = () => {
                   
                   {/* Persona-04 */}
                   <img src="https://images.unsplash.com/photo-1600481176431-47ad2ab2745d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
-                  alt="titanium" className="userStories-video g_grow" />
+                  className="userStories-video g_grow" />
                     <div className="userStories-video-container g_grow flex-1 flex-center" 
                           style={{
                             position: 'absolute',
                             top: '9%',
-                            left: '10%',
+                            left: '8%',
                             right: '25%'
                           }}>
                       <p className="text-zinc userStories-video g_grow italic">
@@ -144,15 +141,15 @@ const UserStories = () => {
                       </p>
                   </div>
                 </div>
-                <div className="overflow-hidden flex-1 h-[100vh]">
+                <div className="overflow-hidden flex-1 h-[100vh] relative">
                   {/* Persona-06 */}
                   <img src="https://images.unsplash.com/photo-1594540637720-9b14714212f4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" 
-                  alt="titanium 2" className="userStories-video g_grow" />
+                  className="userStories-video g_grow" />
                   <div className="userStories-video-container g_grow flex-1 flex-center" 
                           style={{
                             position: 'absolute',
-                            top: '60%',
-                            left: '58%',
+                            top: '75%',
+                            left: '9%',
                             right: '25%'
                           }}>
                       <p className=" userStories-video g_grow italic">
