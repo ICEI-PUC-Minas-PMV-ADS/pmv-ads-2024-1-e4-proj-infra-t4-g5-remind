b869bacc8dfb1f729a93ef7c0a843ad29c097d5f
André Azevedo <aazevedo525@gmail.com>
2024-06-07 12:26:40 -0300

    Remove mobile origin
    remoção da versão inicial do mobile

--- src/Mobile/src/context/UserContextHook.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { UserContext } from '../context/UserContext';
-
-export default function useUser() {
-  const context = useContext(UserContext);
-  const { signed, setSigned, user, setUser, logout } = context;
-  return { signed, setSigned, user, setUser, logout };
-}
